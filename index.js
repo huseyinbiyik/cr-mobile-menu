ee0c7788ab56604d3af6bd2d94b5312898c71dd9
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-11-23 17:11:29 +0300

    Add mobile nav functionality

---
+++ index.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".menu-bars");
+var mobilenav = document.getElementById("mobile-nav");
+button.onclick = function () {
+  mobilenav.style.display = "block";
+};
+
+console.log("Hello World");
