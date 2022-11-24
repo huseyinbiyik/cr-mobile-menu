b8377c3ae2b066edcb19fe6e085432bfffdefe8a
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-11-24 13:15:55 +0300

    Fix linters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var button = document.querySelector(".menu-bars");
-var mobilenav = document.getElementById("mobile-nav");
+const button = document.querySelector('.menu-bars');
+const mobilenav = document.getElementById('mobile-nav');
 button.onclick = function () {
-  mobilenav.style.display = "block";
+  mobilenav.style.display = 'block';
 };
 
-console.log("Hello World");
+console.log('Hello World');
