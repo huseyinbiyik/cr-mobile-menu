280eb64efd4c494e58bb705097ca3ac73c83ba64
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-11-25 10:40:21 +0300

    Fix eslint issues

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const button = document.querySelector(".menu-bars");
-const mobileNavCloseButton = document.querySelector(".nav-close");
-const mobilenav = document.getElementById("mobile-nav");
+const button = document.querySelector('.menu-bars');
+const mobileNavCloseButton = document.querySelector('.nav-close');
+const mobilenav = document.getElementById('mobile-nav');
 button.onclick = function () {
-  mobilenav.style.display = "block";
+  mobilenav.style.display = 'block';
 };
 mobileNavCloseButton.onclick = function () {
-  mobilenav.style.display = "none";
+  mobilenav.style.display = 'none';
 };
 
-console.log("Hello World");
+console.log('Hello World');
